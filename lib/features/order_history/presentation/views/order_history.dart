14f9b8dbbcb3c2b87afe3558acfeb6c18ed07122
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2025-12-05 07:19:21 +0200

    refactor order history view to enhance UI and display order details in a list

--- lib/features/order_history/presentation/views/order_history.dart
+++ lib/features/order_history/presentation/views/order_history.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:foodapp/features/order_history/presentation/widgets/order_card.dart';
+import 'package:go_router/go_router.dart';
 
 class OrderHistory extends StatelessWidget {
   const OrderHistory({super.key});
@@ -6,13 +8,42 @@ class OrderHistory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFF8FFED),
       appBar: AppBar(
-        title: const Text('Order History'),
-        backgroundColor: const Color(0xFF006400),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
+          onPressed: () => context.pop(),
+        ),
+        title: const Text(
+          "Order History",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+          ),
+        ),
+        centerTitle: true,
       ),
-      body: const Center(
-        child: Text('This is the Order History View'),
+      body: ListView.builder(
+        padding: const EdgeInsets.fromLTRB(20, 10, 20, 140), // 140 عشان الـ BottomNavBar ما يغطيش
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return orderCard(
+            orderId: "#123456789",
+            date: "01/01/2023",
+            itemsCount: 2,
+            total: 50.0,
+            status: "Delivered",
+          );
+        },
+        
+        
       ),
+      
     );
   }
+
+  
 }
