017f8ce7a9b04316e744df9732523d2c9b9e0002
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2025-12-05 03:50:00 +0200

    create custom text field

---
+++ lib/features/auth/presentation/widgets/custom_text_form_field.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({
+    super.key,
+    required this.controller,
+    required this.labelText,
+    required this.hintText,
+    required this.prefixIcon,
+    this.suffixIcon,
+    required this.keyboardType,  this.obscureText=false,
+  });
+
+  final TextEditingController controller;
+  final String labelText;
+  final String hintText;
+  final IconData prefixIcon;
+  final Widget? suffixIcon;
+  final TextInputType keyboardType;
+  final bool? obscureText ;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter some text';
+        }
+        return null;
+      },
+      keyboardType: keyboardType,
+      obscureText: obscureText ?? false,
+      decoration: InputDecoration(
+        labelText: labelText,
+        hintText: hintText,
+        prefixIcon: Icon(prefixIcon),
+        suffixIcon: suffixIcon,
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: Color(0xFF006400), width: 1),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: Color(0xFF006400), width: 1),
+        ),
+      ),
+    );
+  }
+}
