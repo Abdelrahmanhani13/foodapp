10364d182fc6c4092c1669d344e4816f004c1ebc
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2025-12-07 04:34:49 +0200

    refactor LoginView to use BlocConsumer for authentication state management and improve user feedback

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,7 +1,10 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:foodapp/core/utils/app_colors.dart';
 import 'package:foodapp/core/utils/app_router.dart';
 import 'package:foodapp/core/utils/text_style.dart';
+import 'package:foodapp/features/auth/presentation/logic/auth_cubit/authentication_cubit.dart';
 import 'package:foodapp/features/auth/presentation/widgets/custom_text_form_field.dart';
 import 'package:go_router/go_router.dart';
 
@@ -21,149 +24,194 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(24),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 40),
-                Center(
-                  child: Container(
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      color: AppColors.primary,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: const Icon(
-                      Icons.fastfood,
-                      size: 50,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 40),
-                Center(
-                  child: Text(
-                    'Welcome Back!',
-                    style: TextStyles.bold16.copyWith(fontSize: 32),
-                  ),
-                ),
-                const SizedBox(height: 8),
-                Center(
-                  child: Text(
-                    'Sign in to continue',
-                    style: TextStyles.regular13.copyWith(
-                      color: AppColors.borderDark,),
-                  ),
-                ),
-                const SizedBox(height: 40),
-                CustomTextFormField(
-                  controller: _emailController,
-                  labelText: 'Email',
-                  hintText: 'Enter your email',
-                  prefixIcon: Icons.email_outlined,
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                const SizedBox(height: 20),
-                CustomTextFormField(
-                  controller: _passwordController,
-                  labelText: 'Password',
-                  hintText: 'Enter your password',
-                  prefixIcon: Icons.lock,
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: obscureText,
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      obscureText ? Icons.visibility_off : Icons.visibility,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        obscureText = !obscureText;
-                      });
-                    },
-                  ),
-                ),
-                const SizedBox(height: 8),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return BlocConsumer<AuthenticationCubit, AuthenticationState>(
+      listener: (context, state) {
+        if (state is LoginFailure) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(state.errorMessage),
+              backgroundColor: Colors.red,
+            ),
+          );
+        } else if (state is LoginSuccess) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Login Successful!'),
+              backgroundColor: Colors.green,
+            ),
+          );
+          context.go(AppRouter.home);
+        } else if (state is LoginLoading) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Logging in...'),
+              backgroundColor: AppColors.primary,
+            ),
+          );
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          backgroundColor: Colors.white,
+          body: SafeArea(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(24),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: TextButton(
-                        onPressed: () {
-                          context.go(AppRouter.forgotPassword);
-                        },
-                        child: Text(
-                          'Forgot Password?',
-                          style: TextStyles.semiBold16.copyWith(
-                            color: AppColors.primary,
-                          ),
+                    const SizedBox(height: 40),
+                    Center(
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: AppColors.primary,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Icon(
+                          Icons.fastfood,
+                          size: 50,
+                          color: Colors.white,
                         ),
                       ),
                     ),
+                    const SizedBox(height: 40),
                     Center(
-                      child: Row(
-                        children: [
-                          Checkbox(
-                            value: _rememberMe,
-                            onChanged: (value) {
-                              setState(() {
-                                _rememberMe = value!;
-                              });
+                      child: Text(
+                        'Welcome Back!',
+                        style: TextStyles.bold16.copyWith(fontSize: 32),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Center(
+                      child: Text(
+                        'Sign in to continue',
+                        style: TextStyles.regular13.copyWith(
+                          color: AppColors.borderDark,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                    CustomTextFormField(
+                      controller: _emailController,
+                      labelText: 'Email',
+                      hintText: 'Enter your email',
+                      prefixIcon: Icons.email_outlined,
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    const SizedBox(height: 20),
+                    CustomTextFormField(
+                      controller: _passwordController,
+                      labelText: 'Password',
+                      hintText: 'Enter your password',
+                      prefixIcon: Icons.lock,
+                      keyboardType: TextInputType.visiblePassword,
+                      obscureText: obscureText,
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          obscureText ? Icons.visibility_off : Icons.visibility,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            obscureText = !obscureText;
+                          });
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: TextButton(
+                            onPressed: () {
+                              context.go(AppRouter.forgotPassword);
                             },
-                            activeColor: const Color(0xFF006400),
+                            child: Text(
+                              'Forgot Password?',
+                              style: TextStyles.semiBold16.copyWith(
+                                color: AppColors.primary,
+                              ),
+                            ),
                           ),
-                          const Text('Agree to Terms & Conditions'),
-                        ],
+                        ),
+                        Center(
+                          child: Row(
+                            children: [
+                              Checkbox(
+                                value: _rememberMe,
+                                onChanged: (value) {
+                                  setState(() {
+                                    _rememberMe = value!;
+                                  });
+                                },
+                                activeColor: const Color(0xFF006400),
+                              ),
+                              const Text('Agree to Terms & Conditions'),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 24),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate() &&
+                              _rememberMe == true &&
+                              state is! LoginLoading) {
+                            context.read<AuthenticationCubit>().login(
+                              _emailController.text.trim(),
+                              _passwordController.text.trim(),
+                            );
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF006400),
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        child: state is LoginLoading
+                            ? const CircularProgressIndicator(
+                                color: Colors.white,
+                              )
+                            : Text(
+                                'Sign In',
+                                style: TextStyles.bold16.copyWith(
+                                  color: Colors.white,
+                                ),
+                              ),
                       ),
+                    ),
+                    const SizedBox(height: 24),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Don\'t have an account? ',
+                          style: TextStyles.regular13,
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            context.go(AppRouter.signup);
+                          },
+                          child: Text('Sign Up', style: TextStyles.semiBold16),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-                const SizedBox(height: 24),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF006400),
-                      padding: const EdgeInsets.symmetric(vertical: 16),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                    ),
-                    child: Text(
-                      'Sign In',
-                      style: TextStyles.bold16.copyWith(color: Colors.white),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 24),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Don\'t have an account? ',
-                      style: TextStyles.regular13,
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        context.go(AppRouter.signup);
-                      },
-                      child: Text('Sign Up', style: TextStyles.semiBold16),
-                    ),
-                  ],
-                ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 
