0c4f705fd7de8c6d236054b9d993cbb42fd6ba94
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2025-12-07 07:23:15 +0200

    refactor AuthRepo to improve error handling and add updateUserProfile method for user profile updates

--- lib/features/auth/data/auth_repo.dart
+++ lib/features/auth/data/auth_repo.dart
@@ -5,46 +5,65 @@ import 'package:foodapp/core/utils/pref_helper.dart';
 import 'package:foodapp/features/auth/data/model/user_model.dart';
 
 class AuthRepo {
-
   final ApiService apiService;
   AuthRepo(this.apiService);
 
   Future<UserModel?> login(String email, String password) async {
-    try{
-      final response =await apiService.postRequest('/login', {
-        'email':email,
-        'password':password,
+    try {
+      final response = await apiService.postRequest('/login', {
+        'email': email,
+        'password': password,
       });
-      final user =UserModel.fromJson(response['data']);
-      if(user.token!=null){
+      final user = UserModel.fromJson(response['data']);
+      if (user.token != null) {
         await PrefHelper.saveToken(user.token!);
       }
       return user;
-    }on DioException catch(e){
-      final failure=ServerFailure.fromDioError(e);
+    } on DioException catch (e) {
+      final failure = ServerFailure.fromDioError(e);
       throw failure;
-    } catch(e){
+    } catch (e) {
       throw ServerFailure('Unexpected Error, Please try again!');
     }
   }
 
-  Future<UserModel?> register(String name, String email, String password) async {
-    try{
-      final response =await apiService.postRequest('/register', {
-        'name':name,
-        'email':email,
-        'password':password,
+  Future<UserModel?> register(
+    String name,
+    String email,
+    String password,
+  ) async {
+    try {
+      final response = await apiService.postRequest('/register', {
+        'name': name,
+        'email': email,
+        'password': password,
       });
-      final user =UserModel.fromJson(response['data']);
-      if(user.token!=null){
+      final user = UserModel.fromJson(response['data']);
+      if (user.token != null) {
         await PrefHelper.saveToken(user.token!);
       }
       return user;
-    }on DioException catch(e){
-      final failure=ServerFailure.fromDioError(e);
+    } on DioException catch (e) {
+      final failure = ServerFailure.fromDioError(e);
       throw failure;
-    } catch(e){
+    } catch (e) {
       throw ServerFailure('Unexpected Error, Please try again!');
     }
   }
+
+  Future<UserModel?> getUserProfile() async {
+    try {
+      final response = await apiService.getRequest('/profile');
+      final user = UserModel.fromJson(response['data']);
+      return user;
+    } on DioException catch (e) {
+      final failure = ServerFailure.fromDioError(e);
+      throw failure;
+    } catch (e) {
+      throw ServerFailure('Unexpected Error, Please try again!');
+    }
+  }
+
+  // في ملف auth_repo.dart
+  
 }
