030c03e0df42866c5114cc663612c1972a157753
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2025-12-07 04:33:57 +0200

    add AuthRepo class for user authentication with login and registration methods

---
+++ lib/features/auth/data/auth_repo.dart
@@ -0,0 +1,50 @@
+import 'package:dio/dio.dart';
+import 'package:foodapp/core/error/failure.dart';
+import 'package:foodapp/core/network/api_service.dart';
+import 'package:foodapp/core/utils/pref_helper.dart';
+import 'package:foodapp/features/auth/data/model/user_model.dart';
+
+class AuthRepo {
+
+  final ApiService apiService;
+  AuthRepo(this.apiService);
+
+  Future<UserModel?> login(String email, String password) async {
+    try{
+      final response =await apiService.postRequest('/login', {
+        'email':email,
+        'password':password,
+      });
+      final user =UserModel.fromJson(response['data']);
+      if(user.token!=null){
+        await PrefHelper.saveToken(user.token!);
+      }
+      return user;
+    }on DioException catch(e){
+      final failure=ServerFailure.fromDioError(e);
+      throw failure;
+    } catch(e){
+      throw ServerFailure('Unexpected Error, Please try again!');
+    }
+  }
+
+  Future<UserModel?> register(String name, String email, String password) async {
+    try{
+      final response =await apiService.postRequest('/register', {
+        'name':name,
+        'email':email,
+        'password':password,
+      });
+      final user =UserModel.fromJson(response['data']);
+      if(user.token!=null){
+        await PrefHelper.saveToken(user.token!);
+      }
+      return user;
+    }on DioException catch(e){
+      final failure=ServerFailure.fromDioError(e);
+      throw failure;
+    } catch(e){
+      throw ServerFailure('Unexpected Error, Please try again!');
+    }
+  }
+}
